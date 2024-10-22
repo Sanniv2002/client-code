b6d275b25b85f347efd793750d612fa462634bbb
sanniv1020 <sanniv@useteleport.com>
2024-10-22 18:41:48 +0530

    escaped

--- components/Modals/PopUp.tsx
+++ components/Modals/PopUp.tsx
@@ -30,7 +30,7 @@ const PopUp = ({ show, onClose, message, onConfirm }: { show: boolean, onClose: 
                             onClick={onConfirm}
                             className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
                         >
-                            Yes, I'm sure
+                            Yes, I&apos;m sure
                         </button>
                         <button
                             onClick={onClose}
